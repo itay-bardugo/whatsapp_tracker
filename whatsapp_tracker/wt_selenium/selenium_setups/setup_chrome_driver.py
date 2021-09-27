217fe3aabee3b4eba44459a1d845c075eb753f9d
itay-bardugo <itaybardugo91@gmail.com>
2021-09-27 19:27:35 +0300

    fixed: read as input
    (cherry picked from commit 0df853b024be08c04b046b8ff309aecefdb249a0)

--- whatsapp_tracker/wt_selenium/selenium_setups/setup_chrome_driver.py
+++ whatsapp_tracker/wt_selenium/selenium_setups/setup_chrome_driver.py
@@ -4,19 +4,19 @@ from selenium import webdriver
 from selenium.common.exceptions import InvalidArgumentException
 from selenium.webdriver.chrome.webdriver import WebDriver
 
-from whatsapp_tracker.configs.default_vars_config import DEFAULT_BROWSER_DRIVER_PATH
 from whatsapp_tracker.configs.paths_config import CHROME_DATA_PATH
 from whatsapp_tracker.mixins.selenium_window_mixin import SeleniumWindowMixin
 from whatsapp_tracker.wt_selenium.selenium_setups.setup_selenium_options import SetupSeleniumOptions
 
 
 class SetupChromeDriver(SeleniumWindowMixin):
-    def __init__(self):
+    def __init__(self, browser_path):
         self._setup_options = SetupSeleniumOptions()
+        self.browser_path = browser_path
 
     def setup(self) -> WebDriver:
         try:
-            driver = webdriver.Chrome(DEFAULT_BROWSER_DRIVER_PATH, options=self._setup_options.setup())
+            driver = webdriver.Chrome(self.browser_path, options=self._setup_options.setup())
             self.maximize_window(driver=driver)
         except InvalidArgumentException:
             shutil.rmtree(CHROME_DATA_PATH)
