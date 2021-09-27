4ba960cef5ef284388bad344f43515648643458c
itay-bardugo <itaybardugo91@gmail.com>
2021-09-27 13:13:59 +0300

    added: mixins
    (cherry picked from commit 398ddee337cce59d109d855ccca457d75311dfda)

--- whatsapp_tracker/mixins/selenium_options_mixin.py
+++ whatsapp_tracker/mixins/selenium_options_mixin.py
@@ -1,3 +1,8 @@
+import os
+
+from whatsapp_tracker.configs.paths_config import ROOT_PATH, CHROME_DATA_PATH
+
+
 class SeleniumOptionMixin:
     @classmethod
     def _add_option(cls, options, option, value):
@@ -5,6 +10,7 @@ class SeleniumOptionMixin:
         return options
 
     @classmethod
-    def add_user_data_dir_option(cls, options, dir='chrome-data'):
-        cls._add_option(options=options, option='--user-data-dir', value=dir)
+    def add_user_data_dir_option(cls, options):
+        cls._add_option(options=options, option='--user-data-dir', value=CHROME_DATA_PATH)
+        cls._add_option(options=options, option="disable-blink-features", value='AutomationControlled')
         return options
