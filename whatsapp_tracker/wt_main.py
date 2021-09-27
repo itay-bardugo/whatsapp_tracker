8abe0768de02a9421643a529306e364ad9b10794
itay-bardugo <itaybardugo91@gmail.com>
2021-09-27 13:14:43 +0300

    updated: entrypoint
    (cherry picked from commit a2e26ee8b4c3d0da47612aad833c2e790c3279ef)

--- whatsapp_tracker/wt_main.py
+++ whatsapp_tracker/wt_main.py
@@ -1,15 +1,26 @@
+import logging
 from argparse import ArgumentParser
 
-from whatsapp_tracker.configs.default_vars_config import DEFAULT_BROWSER_DRIVER_PATH
-from whatsapp_tracker.mains.wt_main import WTMain
+from whatsapp_tracker.configs.default_vars_config import DEFAULT_BROWSER_DRIVER_PATH, DEFAULT_SAMPLE_RESOLUTION, \
+    DEFAULT_MINUTES
+from whatsapp_tracker.mains.whatsapp_tracker_main import WhatsappTrackerMain
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     parser = ArgumentParser()
     parser.add_argument('--browser-path', default=DEFAULT_BROWSER_DRIVER_PATH)
+    parser.add_argument('--contact', required=True)
+    parser.add_argument('--save-path', required=True)
+    parser.add_argument('--resolution', default=DEFAULT_SAMPLE_RESOLUTION, type=int)
+    parser.add_argument('--minutes', default=DEFAULT_MINUTES, type=int)
 
     args = parser.parse_args()
 
-    wt_main = WTMain(
-        browser_path=args.browser_path
+    wt_main = WhatsappTrackerMain(
+        browser_path=args.browser_path,
+        search_input=args.contact,
+        save_path=args.save_path,
+        resolution=args.resolution,
+        minutes=args.minutes,
     )
     wt_main.main()
